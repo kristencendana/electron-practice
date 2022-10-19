3b1ed22bbbe44f66b050ca0dc4786f9be08896f9
Kristen Abby Cendana <kristencendana@gmail.com>
2022-10-19 14:54:31 -0700

    electron, react, typescript, and webpack working

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './electron/electron.ts',
+    target: 'electron-main',
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        include: /electron/,
+        use: [{ loader: 'ts-loader' }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'electron.js'
+    }
+  },
+  {
+    mode: 'development',
+    entry: './client/react.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: { rules: [{
+      test: /\.ts(x?)$/,
+      include: /client/,
+      use: [{ loader: 'ts-loader' }]
+    }] },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'react.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './client/index.html'
+      })
+    ]
+  }
+];
